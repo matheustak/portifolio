e47a29e00fd2dae4e36901b46a1f6fb92e5fdea5
matheus <matheusrochaalecrim@gmail.com>
2024-03-03 10:31:54 -0400

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,18 +13,32 @@ const Home = () => {
   return (
    
 <div className={styles.home}>
-    <div className={ styles.texto}>
+    <div className={ styles.grid2}>
     <p>
     Olá pessoas,</p>
   <h1>EU SOU UM 
         PROGRAMADOR</h1>
         <p>seja bem-vindo ao meu portifólio website</p>
 
+
+        <p>Redes Socias</p>
+
+
+       
+<a href="https://github.com/matheustak" target="_blank" rel="noreferrer">
+   <img  className={styles.git} src={minhaImagem1}  alt="Segua-me no github" />
+</a>
+
+
+<a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2FAlecrimRoc3694.com%2F&amp;ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5AlecrimRoc3694&amp;region=follow_link&amp;screen_name=AlecrimRoc3694" target="_blank" rel="noreferrer"> 
+ <img className={styles.twitter} src={minhaImagem2}  alt="Segua-me no twitter"  />
+</a>
+
         </div>
        
  
 
-    <div className={styles.fotologo}>
+    <div className={styles.grid2}>
     <img className={styles.image} src={minhaImagem}  alt="Imagem de entrada do Web-Site" />
     </div>
 
@@ -33,21 +47,7 @@ const Home = () => {
 
 
     
-    <div className={ `${styles.redes } `}>
-    <p>Redes Socias</p>
-
-
-       
-        <a href="https://github.com/matheustak" target="_blank" rel="noreferrer">
-           <img  className={styles.git} src={minhaImagem1}  alt="Segua-me no github" />
-        </a>
-
-
-        <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2FAlecrimRoc3694.com%2F&amp;ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5AlecrimRoc3694&amp;region=follow_link&amp;screen_name=AlecrimRoc3694" target="_blank" rel="noreferrer"> 
-         <img className={styles.twitter} src={minhaImagem2}  alt="Segua-me no twitter"  />
-        </a>
-       
-        </div>
+    
 
         <WhatsAppFlutuante />
 </div>
