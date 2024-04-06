0627d9b4e12d1849b9aebfc0c9d907da1ec246d3
matheus <matheusrochaalecrim@gmail.com>
2024-04-06 14:33:36 -0400

    update blog

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,5 +1,5 @@
 import styles from "./Blog.module.css";
-
+import WhatsAppFlutuante from  "../../components/WhatsAppFlutuante.js";
 const Blog = () => {
   return (
     <div className={styles.Blog}>Blog
