3bf05ee213af821a7964762ce845d5e48a55a06a
matheus <matheusrochaalecrim@gmail.com>
2024-04-06 16:29:54 -0400

    update

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import styles from "./Blog.module.css";
 import WhatsAppFlutuante from  "../../components/WhatsAppFlutuante.js";
+
 const Blog = () => {
   return (
-    <div className={styles.Blog}>Blog
-
-<WhatsAppFlutuante />
+    <div className={styles.blog}>
+    
+      <iframe
+        src="https://mainmatheusdevportifolio.wordpress.com/2024/04/06/salmos/" 
+        title="Blog" 
+        className={styles.iframe}
+      
+        sandbox="allow-same-origin allow-scripts"
+      ></iframe>
+      <WhatsAppFlutuante />
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
