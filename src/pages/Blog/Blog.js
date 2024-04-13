6eddeb46acb54af4d79c936b5d8202eb15a2d9a0
matheus <matheusrochaalecrim@gmail.com>
2024-04-13 19:00:07 -0400

    update

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -6,8 +6,9 @@ const Blog = () => {
   return (
     <div className={styles.blog}>
     
-      <iframe
-        src="https://mainmatheusdevportifolio.wordpress.com/2024/04/08/criando-um-componente-flutuante-do-whatsapp-com-animacoes-em-react-usando-css-modules/" 
+   
+<iframe src="https://mainmatheusdevportifolio.wordpress.com/wp-json/oembed/1.0/embed?url=https://mainmatheusdevportifolio.wordpress.com/2024/04/08/criando-um-componente-flutuante-do-whatsapp-com-animacoes-em-react-usando-css-modules/" 
+
         title="Blog" 
         className={styles.iframe}
       
